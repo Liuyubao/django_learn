12083020dd807b73a4728a5ed56f6a79a246bbec
Yubao Liu <louis.liu@bytedance.com>
2021-03-01 19:30:22 -0500

    learn django rest api

---
+++ myProject/api/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Student
+
+
+@admin.register(Student)
+class BlogTypeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')  # 在后台列表下显示的字段
